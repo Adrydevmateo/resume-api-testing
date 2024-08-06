f514e31fc4056783007f1bfdceac0ec45ce6611d
AdryDev <adrydevmateo@gmail.com>
2024-08-05 23:29:35 -0400

    Initial Commit

---
+++ modules/user/index.ts
@@ -0,0 +1,74 @@
+import { Database } from "bun:sqlite";
+import type { TUser } from "./user.types";
+
+export class User {
+  constructor(private db: Database) {}
+
+  createTableIfNotExists() {
+    const query = this.db.query(
+      `CREATE TABLE IF NOT EXISTS User (
+        id TEXT PRIMARY KEY,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL
+      )`,
+    );
+
+    query.run();
+    query.finalize();
+  }
+
+  dropTableIfExists() {
+    const query = this.db.query("DROP TABLE IF EXISTS User");
+    query.run();
+    query.finalize();
+  }
+
+  getUsers() {
+    const query = this.db.query("SELECT * FROM User");
+    const result = query.all();
+    return result;
+  }
+
+  getUserById(id: string) {
+    const query = this.db.query("SELECT * FROM User WHERE id = $id");
+    const result = query.get({ id });
+    return result;
+  }
+
+  insertUsers(users: Array<TUser>) {
+    const query = this.db.prepare(
+      "INSERT INTO User (id, email, password) VALUES ($id, $email, $password)",
+    );
+
+    const transaction = this.db.transaction((users) => {
+      for (const user of users) query.run(user);
+    });
+
+    transaction(users);
+
+    console.log(`Inserted ${users.length} records!`);
+  }
+
+  updateUser(user: TUser) {
+    const query = this.db.query(`
+      UPDATE User
+      SET email = $email, 
+          password = $password
+      WHERE id = $id
+    `);
+
+    const result = query.run(user);
+    let msg = "User not found";
+    if (result.changes) msg = `User with id(${user.id}) successfully updated!`;
+    console.log(msg);
+  }
+
+  deleteUser(id: string) {
+    const query = this.db.query("DELETE FROM User WHERE id = $id");
+    const result = query.run({ id });
+    query.finalize();
+    let msg = `User with id(${id}) was not found`;
+    if (result.changes) msg = `User with id(${id}) has been deleted!`;
+    console.log(msg);
+  }
+}
---
+++ server.ts
@@ -0,0 +1,97 @@
+import { Database } from "bun:sqlite";
+import { User } from "./modules/user";
+
+const db = new Database("./ResumeDB.sqlite", { strict: true });
+
+const user = new User(db);
+
+//user.createTableIfNotExists();
+
+//user.dropTableIfExists();
+
+//user.insertUsers([
+//  {
+//    id: crypto.randomUUID(),
+//    email: "dante@dmc.com",
+//    password: "123",
+//  },
+//  {
+//    id: crypto.randomUUID(),
+//    email: "vergil@dmc.com",
+//    password: "213",
+//  },
+//  {
+//    id: crypto.randomUUID(),
+//    email: "nero@dmc.com",
+//    password: "321",
+//  },
+//  {
+//    id: crypto.randomUUID(),
+//    email: "v@dmc.com",
+//    password: "1",
+//  },
+//]);
+
+//user.deleteUser("");
+
+//user.updateUser({
+//  id: "",
+//  email: "vedited@dmc.edited",
+//  password: "1",
+//});
+
+//user.getUsers();
+
+//db.close();
+
+// TODO: Create a collection of tokens to add a token to every user
+const isAuthorized = (headers: Headers) => headers.get("authorization");
+
+const server = Bun.serve({
+  async fetch(req: Request) {
+    if (!isAuthorized(req.headers))
+      return new Response("Unauthorized", { status: 401 });
+
+    const { method } = req;
+    const { pathname } = new URL(req.url);
+
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.search);
+
+    if (method === "GET" && pathname === "/api/users") {
+      const users = user.getUsers();
+      if (!users) return new Response("No users found");
+      return new Response(JSON.stringify(users), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    if (method === "GET" && pathname.includes("/api/get_user")) {
+      const id = searchParams.get("id");
+      if (!id) return new Response("User id not provided", { status: 404 });
+
+      const found = user.getUserById(id);
+      if (!found) return new Response("User not found", { status: 404 });
+
+      return new Response(JSON.stringify(found), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    if (method === "POST" && pathname === "/api/create_user") {
+      // Create user
+    }
+
+    if (method === "PATCH" && pathname === "/api/update_user") {
+      // Update user
+    }
+
+    if (method === "DELETE" && pathname === "/api/delete_user") {
+      // Delete user
+    }
+
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log("Running on port:", server.port);
